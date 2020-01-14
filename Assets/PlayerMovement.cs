44688fe436b1bbea79e17eb8546a1546ae1b73b1
axel9674 <axel9674@gmail.com>
2020-01-14 19:45:33 +0100

    Basic movement using touch pads: left / right movement, jumping, crouching

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Joystick horizontalJoystick;
+    public Joystick verticalJoystick;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    public float runSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = horizontalJoystick.Horizontal * runSpeed;
+
+        /*
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        */
+        if (verticalJoystick.Vertical == 1)
+        {
+            jump = true;
+        }
+        /*
+       if (Input.GetButtonDown("Crouch"))
+       {
+           crouch = true;
+       } else if (Input.GetButtonUp("Crouch"))
+       {
+           crouch = false;
+       }
+       */
+        if (verticalJoystick.Vertical == -1)
+        {
+            crouch = true;
+        }
+        else if (verticalJoystick.Vertical != -1)
+        {
+            crouch = false;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+}
